3d8e20d86229138a7650a662c3a5569dc15e4d45
Pedro Augusto <pn.alegrete@hotmail.com>
2018-03-08 03:24:09 +0000

    minimap done

---
+++ Assets/MinimapFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+}
