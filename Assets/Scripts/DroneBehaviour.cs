cef4a1161ab17e8fbce6daeedff86f7d287a2bff
Pedro Augusto <pn.alegrete@hotmail.com>
2018-03-15 05:22:24 +0000

    almost done
    rasgos de genialidade

--- Assets/Scripts/DroneBehaviour.cs
+++ Assets/Scripts/DroneBehaviour.cs
@@ -6,26 +6,24 @@ namespace GameControll
 {
     public class DroneBehaviour : MonoBehaviour
     {
+        public CameraManager camera;
         Vector3 startingPosition;
         public float movementSpeed;
         public float turnSpeed;
-        float speed;
 
         public Rigidbody player;
         Rigidbody droneRigid;
-        public CameraManager camera;
         public StateManager playerState;
 
-        bool gotTooFarAway;
+        public bool colLeft;
+        public bool colRight;
+        public bool colFront;
+        public bool colBack;
+        public bool colUp;
+        public bool colDown;
 
-        Vector3 nextPosition;
         Vector3 yPlaneView;
-
-        Vector3 storeTarget;
-        Vector3 newTargetPos;
-        bool savePos;
-        bool overrideTarget;
-        Transform obstacle;
+        
         public List<Vector3> EscapeDirections = new List<Vector3>();
         Vector3 lastVelocity;
 
@@ -33,7 +31,7 @@ namespace GameControll
 
         private void Start()
         {
-            gotTooFarAway = false;
+            
             yPlaneView = new Vector3(0, player.position.y + 2.5f, 0);
             startingPosition = player.position - player.transform.right + new Vector3(0, 2f, 0);
             droneRigid = GetComponent<Rigidbody>();
@@ -91,96 +89,75 @@ namespace GameControll
             */
             #endregion
             
-            int avoidCount;
+            //if player nao estiver no estado de andar nao mudar
+            //raycast no vetor velocidade
+            //redirecionar velocidade para uma tangente a parede, retirando como feito antes as componentes nessa direção, e aplicando
+            //a magnitude da velocidade igual. quoficiente entre a magnitude normal e a after collision
 
-            avoidCount = 0;
+            startingPosition = player.position - player.transform.right - player.transform.forward + new Vector3(0, 1.5f, 0);
 
             
-
-            
-            RaycastHit[] hit = Rays(-player.transform.right);
-            for (int i = 0; i < hit.Length - 1; i++)
-            {
-                if (hit[i].transform.root.gameObject != this.gameObject && hit[i].transform.root.gameObject != player)
-                {
-                    avoidCount++;
-                }
-            }
-
-            hit = Rays(player.transform.up);
-            for (int i = 0; i < hit.Length - 1; i++)
-            {
-                if (hit[i].transform.root.gameObject != this.gameObject && hit[i].transform.root.gameObject != player)
-                {
-                    avoidCount++;
-                }
-            }
-
-            hit = Rays(player.transform.forward);
-            for (int i = 0; i < hit.Length - 1; i++)
-            {
-                if (hit[i].transform.root.gameObject != this.gameObject && hit[i].transform.root.gameObject != player)
-                {
-                    
-                    avoidCount++;
-                }
-            }
-
-            if(avoidCount != 0)
-            {
-                startingPosition = player.position - player.transform.forward + transform.right + new Vector3(0, 1.5f, 0);
-            }
-            else
-            {
-                startingPosition = player.position - player.transform.right - player.transform.forward + new Vector3(0, 1.5f, 0);
-            }
-            
-           
-            
-            
-            //Vector3 attractionDir = startingPosition - droneRigid.position;
-
-
-            //droneRigid.velocity = attractionDir.normalized  + new Vector3(player.velocity.x, 0, player.velocity.z);
-            //droneRigid.AddForce(attractionDir.normalized * 10, ForceMode.Force);
-
-            //droneRigid.AddForceAtPosition(attractionDir.normalized, startingPosition, ForceMode.Force);
-
-            Vector3 velocityDrone = new Vector3(player.velocity.x, 0, player.velocity.z) + ((startingPosition - droneRigid.position)/ Time.deltaTime);
+            Vector3 velocityDrone = (startingPosition - droneRigid.position) / Time.deltaTime * 0.2f;
             droneRigid.velocity = velocityDrone;
-            
 
             
+            //Soluçao: starting position muda com as colisões, e não a velocidade do drone
 
-            /*
-            Vector3 acceleration = attractionDir / droneRigid.mass;
-            lastVelocity = droneRigid.velocity;
+            
+            colFront = Physics.Raycast(transform.position, droneRigid.velocity, 2f);
 
-            if(acceleration == Vector3.zero)
+            
+
+            if (colFront)
             {
-                droneRigid.velocity = Vector3.zero;
-                droneRigid.angularVelocity = Vector3.zero;
+                //rotate velocity vector to accompany collision
+                droneRigid.velocity -= droneRigid.velocity;
             }
-            //droneRigid.position = Vector3.Lerp(droneRigid.position, startingPosition, Time.deltaTime);
+            /*
+            colFront = Physics.Raycast(transform.position, player.transform.forward, 1f);
+            colBack = Physics.Raycast(transform.position, -player.transform.forward, 1f);
+            colLeft = Physics.Raycast(transform.position, -player.transform.right, 1f);
+            colRight = Physics.Raycast(transform.position, player.transform.right, 1f);
+            colUp = Physics.Raycast(transform.position, player.transform.up, 1f);
+            colDown = Physics.Raycast(transform.position, -player.transform.up, 1f);
+
+            if (colLeft)
+            {
+                droneRigid.velocity += Vector3.Scale(player.transform.right, droneRigid.velocity);
+            }
+            else if (colRight)
+            {
+                droneRigid.velocity -= Vector3.Scale(player.transform.right, droneRigid.velocity);
+            }
+
+
+
+            if (colFront)
+            {
+                droneRigid.velocity -= Vector3.Scale(player.transform.forward, droneRigid.velocity);
+            }
+            else if (colBack)
+            {
+                droneRigid.velocity += Vector3.Scale(player.transform.forward, droneRigid.velocity);
+            }
+
+            if (colUp)
+            {
+                droneRigid.velocity -= Vector3.Scale(player.transform.up, droneRigid.velocity);
+            }
+            else if (colDown)
+            {
+                droneRigid.velocity += Vector3.Scale(player.transform.up, droneRigid.velocity);
+            }
             */
+
             this.transform.rotation = camera.transform.rotation;
-            Debug.Log(player.velocity.x);
-            Debug.Log(player.velocity.z);
+            
 
         }
 
 
-        RaycastHit[] Rays(Vector3 direction)
-        {
-            Ray ray = new Ray(transform.position, direction);
-            Debug.DrawRay(transform.position, direction * 10 , Color.red);
-
-            float distanceToLookAhead = 5;
-
-            RaycastHit[] hits = Physics.SphereCastAll(ray,1, distanceToLookAhead);
-
-            return hits;
-        }
+       
     }
 }
 
