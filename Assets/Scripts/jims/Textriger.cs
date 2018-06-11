5e56b04a7f72de6bddab3edaf7fbbf96483c0551
M Jimmy <mikejimmyalmeida@gmail.com>
2018-06-11 23:52:34 +0100

    V0.3

---
+++ Assets/Scripts/jims/Textriger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class Textriger : MonoBehaviour {
+
+        public GameObject UInfoDisplay;
+        private void Start()
+        {
+            UInfoDisplay.SetActive(false);
+        }
+
+        void OnTriggerEnter(Collider playr)
+        {
+
+            UInfoDisplay.SetActive(true);
+
+        }
+        void OnTriggerExit(Collider playr)
+        {
+                UInfoDisplay.SetActive(false);
+        }
+    }
+
+
---
+++ Assets/Scripts/jims/menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour
+
+{
+    public Button playBtn;
+    public Button optionsBtn;
+    public Button exitBtn;   
+    public Button controlsBtn;
+    public Button aboutGameBtn;
+    public Button backBtn;
+    public GameObject controlsBtnReveal;
+    public GameObject aboutBtnReveal;
+
+    void Start () {
+
+        controlsBtnReveal.gameObject.SetActive(false);
+        aboutBtnReveal.gameObject.SetActive(false);
+        playBtn.onClick.AddListener(() => { SceneManager.LoadScene(4); });
+        optionsBtn.onClick.AddListener(() => {controlsBtnReveal.gameObject.SetActive(true); aboutBtnReveal.gameObject.SetActive(true); });
+        exitBtn.onClick.AddListener(() => {  Application.Quit(); });
+        controlsBtn.onClick.AddListener(() => { SceneManager.LoadScene(3); });
+        aboutGameBtn.onClick.AddListener(() => { SceneManager.LoadScene(2); });
+        backBtn.onClick.AddListener(() => { SceneManager.LoadScene(1); });
+    }
+	
+	void Update () {          
+	}
+}
